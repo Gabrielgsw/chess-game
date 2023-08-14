dc307286194200ded51a5cdfad54045c5ebcb96e
Gabrielgsw <ggermanow279@gmail.com>
2023-08-14 18:09:01 -0300

    fazendo a lógica do movimento das peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,32 @@
 package application;
 
+import java.util.Scanner;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPosition;
 import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		// Passando a matriz de peças como argumento para a classe User Interface no método printBoard
-		UI.printBoard(chessMatch.getPieces());
+		while (true) {
+			// Passando a matriz de peças como argumento para a classe User Interface no método printBoard
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		
 	}
 
 }
